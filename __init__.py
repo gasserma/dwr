e598b39c0f9a278af02547cc6722407cf5787561
gasserma <gasserma@gmail.com>
2017-03-24 22:52:28 -0700

    I sure hope no one ever looks at this commit history.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hi Bean!'
+def son():
+    return 'Hi Adam!'
 
+@app.route('/ermy')
+def wife():
+    return 'Hi Love!'
+
+@app.route('/cerble')
+def mom():
+    return 'Hi Mom!'
+
+@app.route('/reeech')
+def dad():
+    return 'Hi Dad!'
