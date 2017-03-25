ebcaa346c9e4d2b9f2b0d603bda10e33bcbdf848
gasserma <gasserma@gmail.com>
2017-03-25 12:04:24 -0700

    Don't know why this wasn't immediately obvious...

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ The flask application package.
 """
 
 from flask import Flask
-app = Flask(__name__, template_folder="./FlaskWebServer/templates")
+app = Flask(__name__, template_folder="./FlaskWebServer/templates", static_folder="./FlaskWebServer/static")
 
 from datetime import datetime
 from flask import render_template
@@ -11,33 +11,40 @@ from flask import render_template
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Hi Emily',
-        year=datetime.now().year,
-    )
+    try:
+        """Renders the home page."""
+        return render_template(
+            'index.html',
+            title='Hi Emily',
+            year=datetime.now().year,
+        )
+    except Exception as e:
+        return e.__str__()
 
 @app.route('/calc')
 def calc():
-    retirementLength = 30
-    initialPortfolio = 2 * 1000 * 1000
+    try:
+        retirementLength = 30
+        initialPortfolio = 2 * 1000 * 1000
 
-    from Engine.simulation import runSimulation
-    from Engine.strategies.guyton_klinger import GuytonKlinger
-    from Engine.assets import Assets
-    result = runSimulation(
-       retirementLength,
-       initialPortfolio,
-       .055 * initialPortfolio * .5,
-       (
-           (GuytonKlinger(.055 * initialPortfolio, retirementLength), Assets(.5, .5), 1.0),
-       ),
-       1926,
-       2010
-    )
+        from Engine.simulation import runSimulation
+        from Engine.strategies.guyton_klinger import GuytonKlinger
+        from Engine.assets import Assets
+        result = runSimulation(
+            retirementLength,
+            initialPortfolio,
+            .055 * initialPortfolio * .5,
+            (
+                (GuytonKlinger(.055 * initialPortfolio, retirementLength), Assets(.5, .5), 1.0),
+            ),
+            1926,
+            2010
+        )
+        return result.__str__()
+    except Exception as e:
+        return e.__str__()
 
-    return "actually ran a simulation"
+
 
 @app.route('/test')
 def test():
