0bae5e7d2f98de39843abb7cad18d3b84806db2b
gasserma <gasserma@gmail.com>
2017-03-24 23:18:54 -0700

    High probability we are undoing this.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+
+#from simulation import runSimulation
+from strategies.test_trinity import *
+from strategies.guyton_klinger import *
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,3 +21,21 @@ def mom():
 @app.route('/reeech')
 def dad():
     return 'Hi Dad!'
+
+@app.route('/calc')
+def calc():
+    retirementLength = 30
+    initialPortfolio = 2 * 1000 * 1000
+
+    result = runSimulation(
+        retirementLength,
+        initialPortfolio,
+        .055 * initialPortfolio * .5,
+        (
+            (GuytonKlinger(.055 * initialPortfolio, retirementLength), Assets(.5, .5), 1.0),
+        ),
+        1926,
+        2010
+    )
+
+    return result.__str__()
